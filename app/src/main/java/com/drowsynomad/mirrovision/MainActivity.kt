1a6e992d346fea33bbc7c82fbf1bc5fc1df7e242
Roman Voloshyn <roman.voloshyn@nerdpeople.com>
2024-06-19 20:18:04 +0300

    Initial commit

---
+++ app/src/main/java/com/drowsynomad/mirrovision/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.drowsynomad.mirrovision
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.drowsynomad.mirrovision.ui.theme.MirrovisionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MirrovisionTheme {
+
+            }
+        }
+    }
+}
